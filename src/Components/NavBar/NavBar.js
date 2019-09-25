c7371fa8ea68dc867f8a43b8a93b10417dab9b71
admin <woyongegbara@yahoo.com>
2019-09-25 20:32:56 +0200

    Implemented basic NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
+import './navBar.styles.css';
+import DropDownMenu from "./dropDownMenu/dropDownMenu";
+
 
 const NavBar = () => {
   return (
-    <div>
-      <NavLink to={'/'}>Home</NavLink>
-      <NavLink to={'/LogIn'}>Log In</NavLink>
-      <NavLink to={'/SignUp'}>SignUp</NavLink>
-      <NavLink to={'/ViewProfile'}>View Profile</NavLink>
-      <NavLink to={'/SingleProducts'}>Single Products</NavLink>
-      <NavLink to={'/EditProfile'}>Edit Profile</NavLink>
-      <NavLink to={'/Cart'}>Cart</NavLink>
-      <NavLink to={'/Checkout'}>Checkout</NavLink>
-      <NavLink to={'/AllProducts'}>All Products</NavLink>
+    <div className='container'>
+      <div className='links'>
+      <div className='icon'>
+        <NavLink className='navLinks' to={'/'}><img src={require('./icons8-biohazard-40.png')} alt='Home' className='cart'/></NavLink>
+      </div>
+      <div className='middleLinks'>
+        <NavLink className='navLinks' to={'/'}>Home</NavLink>
+      {/*  /!*<NavLink className='navLinks' to={'/SingleProducts'}>Single Products</NavLink>*!/*/}
+        <NavLink className='navLinks' to={'/AllProducts'}>Shop</NavLink>
+      </div>
+      <div className='sideLinks'>
+        {/*<NavLink className='navLinks' to={'/SignUp'}>SignUp</NavLink>*/}
+        {/*<NavLink to={'/EditProfile'}>Edit Profile</NavLink>*/}
+        <NavLink to={'/Cart'}><img src={require('./icons8-checkout-64.png')} alt='Cart' className='cart'/></NavLink>
+        {/*<NavLink className='navLinks' to={'/Checkout'}>Checkout</NavLink>*/}
+        {/*<NavLink className='navLinks' to={'/LogIn'}>Log In</NavLink>*/}
+        <DropDownMenu />
+      </div>
+      </div>
     </div>
+
   )
 };
 
